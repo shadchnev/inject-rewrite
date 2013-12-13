b1f85c03dade565de4958024970f7946c002d6f0
Jeremy Marer <jeremymarer@gmail.com>
2013-12-13 09:23:59 +0000

    new test for subtraction. got it working with setting the range to start at one rather than the first value

--- lib/array.rb
+++ lib/array.rb
@@ -2,7 +2,7 @@ class Array
 
   def shoot &block
     result = self.first
-    for i in result..self.length-1
+    for i in 1..self.length-1
       result = yield result, self[i]
     end
     result
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -9,5 +9,9 @@ describe Array do
     it "multiply all the values in the array [1,2,3,4,5]" do
       expect([1,2,3,4,5].shoot { |sum, number| sum * number}).to eq(120)
     end
+    
+    it "subtract all the values in the array [1000,100,10,1]" do
+      expect([1000,100,10,1].shoot { |sum, number| sum - number}).to eq(889)
+    end
   end
 end
