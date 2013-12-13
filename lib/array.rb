bcfb76f6fb382917e74b5fa84552926c8e632ef2
Jeremy Marer <jeremymarer@gmail.com>
2013-12-13 11:26:10 +0000

    not the best way to handle the number of iterations but I dont know any other way

--- lib/array.rb
+++ lib/array.rb
@@ -2,7 +2,8 @@ class Array
 
   def shoot args=nil, &block
     result = args.nil? ? self.first : args
-    for i in 1..self.length-1
+    start = args.nil? ? 1 : 0
+    for i in start..self.length-1
       result = yield result, self[i]
     end
     result
